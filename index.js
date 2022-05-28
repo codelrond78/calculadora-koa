8ce237dcbdfd6eeb95c5417e1eab27e4f6e7819b
codelrond78 <90870337+codelrond78@users.noreply.github.com>
2022-05-28 06:03:35 +0000

    ;)

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const json = require('koa-json')
 const Koa = require('koa')
+const cors = require('@koa/cors');
+
 const app = new Koa()
 
+app.use(cors());
 app.use(json())
 
 app.use((ctx) => {
   ctx.body = { foo: 'bar' }
 })
+
+app.listen(3000);
